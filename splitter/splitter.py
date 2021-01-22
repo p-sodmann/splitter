74369658869ac757a5142f18292d89b176a686b1
Phil <psodmann@gmail.com>
2021-01-22 13:48:40 +0100

    added simple splitting

--- splitter/splitter.py
+++ splitter/splitter.py
@@ -3,7 +3,7 @@ from collections import defaultdict
 import pickle
 
 class Splitter:
-    def __init__(self, k_splits:int):
+    def __init__(self, k_splits:int=10, verbose=True, seed=None):
         """
         Class to load and split into training and testing into k_splits folds.
         Can save state and add more data later.
@@ -16,23 +16,62 @@ class Splitter:
         # e.g. {0: [a,d,e], 1:[b,c,f]}
         self.splits = defaultdict(list)
         
+        # keep track of already processed items to avoid duplicates
+        self.processed = []
+
         # keep track how many items are in each split
         self.split_lengths = np.zeros(k_splits)
+
+        # if you want me to be silent init with verbose = False
+        self.verbose = verbose
+
+        # dont forget to optimize the seed ;D
+        self.seed = seed
+        self.rng = np.random.default_rng(seed = self.seed)
     
     def add(self, item):
-        # reduce probability for splits with more data
-        distribution = ((sum(self.split_lengths) + 1) / (self.split_lengths + 1)) / self.k_splits
-        probabilities = distribution / sum(distribution)
+        if item not in self.processed:
+            # reduce probability for splits with more data
+            distribution = ((sum(self.split_lengths) + 1) / (self.split_lengths + 1)) / self.k_splits
+            probabilities = distribution / sum(distribution)
+            
+            # chose a split depending on the probabilities
+            split = self.rng.choice(self.k_splits, 1, p=probabilities)[0]
+            
+            # add item to the split
+            self.splits[split].append(item)
+            
+            # update the split_lengths
+            self.split_lengths[split] += 1
+
+            self.processed.append(item)
+
+        else:
+            if self.verbose:
+                print(f"item {item} already processed.")
+
+    def get_split(self, group_indices=[[0,1,2,3,4,5,6,7],[8],[9]]):
+        """
+        group_indices: List(List(split_index))
         
-        # chose a split depending on the probabilities
-        split = np.random.choice(self.k_splits, 1, p= probabilities)[0]
+        example: [[0,1,2,3,4,5,6,7],[8],[9]]
+        for the first 8 splits in the first list, then 1 for training and 1 for testing
         
-        # add item to the split
-        self.splits[split].append(item)
-        
-        # update the split_lengths
-        self.split_lengths[split] += 1
-        
+        returns a list with the corresponding splits inside.
+        """
+        return_data = []
+
+        for group in group_indices:
+            split_data = []
+            
+            for split in group:
+                split_data += self.splits[split]
+                
+            return_data.append(split_data)
+
+        return return_data
+
+
     def save(self, path):
         # save state to path
         with open(path, 'wb') as handle:
